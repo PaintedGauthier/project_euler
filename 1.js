4518730aa73c34fc2e571f992ac65c919e4e61b9
PaintedGauthier <paintedgauthier@gmail.com>
2019-02-15 18:31:28 -0500

    Update 1.js
    Added generic timing and results

--- 1.js
+++ 1.js
@@ -1,5 +1,7 @@
-let fin = 0;
+let t = performance.now(),
+	fin = 0;
 for (let i = 0; i < 1000; ++i)
 	if (i % 3 == 0 || i % 5 == 0) 
-    fin+= i;
-console.log('answer:',fin);
+		fin+= i;
+console.log('answer:',fin,'in (ms) ', (performance.now() - t).toFixed(4));
+/* answer: 233168 in (ms)  0.0450 */
