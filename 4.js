c7cd6192e17e0a48e08cfc0354a5d4982927f07a
PaintedGauthier <paintedgauthier@gmail.com>
2019-02-16 17:59:31 -0500

    Update 4.js
    well miserably failed at that.

--- 4.js
+++ 4.js
@@ -1,32 +1,28 @@
-
 var t = performance.now(),
-	x = 999,
-	y = 999,
+	list = [],
 	temp = 0,
 	answer = 0,
 	orginal = 0,
 	flipped = 0;
 
-	function is_pali(num) {
-		temp = 0;
-		orginal = num;	
-		flipped = 0;
-		while(num > 0) {
-			temp = num % 10;
-			num = parseInt(num / 10);
-			flipped = flipped * 10 + temp;
-		}
-		return flipped == orginal;
+function is_pali(num) {
+	// lets not redeclare vars everytime
+	temp = 0;
+	orginal = num;	
+	flipped = 0;
+	while(num > 0) {
+		temp = num % 10;
+		num = parseInt(num / 10);
+		flipped = flipped * 10 + temp;
 	}
+	return flipped == orginal;
+}
 	
-	while (x > 99) {
-		answer = x * y;
-		if(is_pali(answer)) break;
-		y--;
-		if (y > 100) continue;
-		x--;
-		y = 999;
-	}
-// avg: 1.1012 | min: 0.635 | max: 4.435
+for (var x = 999;x > 99 && answer == 0; --x) 
+for (var y = 999;y > 99 && answer == 0; --y) 
+	if (is_pali(x * y)) 
+		list.push(x * y);
+answer = Math.max(...list)
+			
 console.log(`answer: ${answer} in (ms) ${(performance.now() - t).toFixed(4)}`);
-// answer: 580085 in (ms) 5.0700
+// answer: 906609 in (ms) 145.9200 // answer: 580085 in (ms) 5.0700
